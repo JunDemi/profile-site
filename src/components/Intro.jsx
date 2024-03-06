03770d5ac2af00b5a2ab84a1413ad06b9373a29d
JunDemi <jungwook3176@gmail.com>
2024-03-06 18:24:00 +0900

    Style: 내 기술 스택

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import React, { useRef } from "react";
 
 const Intro = () => {
@@ -10,20 +11,60 @@ const Intro = () => {
     <>
       <div className="intro-container">
         <div>
-          <img src="/bannerbg.jpeg" alt="" />
-          <h1>Hi. I'm</h1>
-          <h2>Frontend Developer.</h2>
+          <motion.img
+            src="/bannerbg.jpeg"
+            alt=""
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.5 }}
+          />
+          <motion.h1
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6 }}
+          >
+            Hi. I'm
+          </motion.h1>
+          <motion.h2
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.6 }}
+          >
+            Front-End Developer.
+          </motion.h2>
+          <motion.p
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.0 }}
+          >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
+            corrupti, facilis fugit quam voluptate omnis, impedit id nisi
+            voluptas cupiditate autem numquam quo odio laudantium rerum,
+            perferendis in excepturi nobis.
+          </motion.p>
+          <motion.button
+            onClick={handleClick}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1.0 }}
+          >
+            Read More
+          </motion.button>
+        </div>
+      </div>
+
+      <div className="my-skill" ref={ref}>
+        <div>
+          <h1>My Skills</h1>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae
             corrupti, facilis fugit quam voluptate omnis, impedit id nisi
             voluptas cupiditate autem numquam quo odio laudantium rerum,
             perferendis in excepturi nobis.
           </p>
-          <button onClick={handleClick}>Read More</button>
         </div>
+        <img src="/bannerbg.jpeg" alt="" />
       </div>
-
-      <div className="nono" ref={ref}>dsadasd</div>
     </>
   );
 };
