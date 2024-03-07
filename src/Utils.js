4824255d1c6ba238f39e0494d2937116c1a31ad2
JunDemi <jungwook3176@gmail.com>
2024-03-07 19:55:41 +0900

    Feat: 자기소개 텍스트 슬라이드

---
+++ src/Utils.js
@@ -0,0 +1,5 @@
+export const introduceText = [
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae corrupti, facilis fugit quam voluptate omnis, impedit id nisi voluptas cupiditate autem numquam quo odio laudantium rerum, perferendis in excepturi nobis.",
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae corrupti, facilis fugit quam voluptate omnis, impedit id nisi voluptas cupiditate autem numquam quo odio laudantium rerum, perferendis in excepturi nobis.",
+  "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae corrupti, facilis fugit quam voluptate omnis, impedit id nisi voluptas cupiditate autem numquam quo odio laudantium rerum, perferendis in excepturi nobis.",
+];
