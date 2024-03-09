88066fd518829f5779cf32912784cb7b8ebbc66e
JunDemi <jungwook3176@gmail.com>
2024-03-09 15:03:38 +0900

    Style: 포트폴리오 슬라이드 디자인

--- src/Utils.js
+++ src/Utils.js
@@ -6,24 +6,34 @@ export const introduceText = [
 
 export const portfolioList = [
   {
-    viewImg: "/bannerbg.jpeg",
-    siteName: "The Marktet",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
+    siteName: "The Market",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
-      "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
-      "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
-      "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
+      "https://camo.githubusercontent.com/27e34bcb27e231767f7d1671498d48181bb3ba244d3876abb15b3be2aeae6bfb/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4e6578742e6a732031342d3030303030303f7374796c653d666f722d7468652d6261646765266c6f676f3d6e657874646f746a73266c6f676f436f6c6f723d666666666666",
+      "https://camo.githubusercontent.com/0bd8a63ca14821421455199e86267172d5dad9860859b46feaf71b32fee86245/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f547970655363726970742d3331373843363f7374796c653d666f722d7468652d6261646765266c6f676f3d74797065736372697074266c6f676f436f6c6f723d666666666666",
+      "https://camo.githubusercontent.com/53101dab446bf07ce2ac9f8c104931e6c359650ef6716a2a5ef25388d621aaa3/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f46697265626173652d4646434132383f7374796c653d666f722d7468652d6261646765266c6f676f3d6669726562617365266c6f676f436f6c6f723d303030303030",
+      "https://camo.githubusercontent.com/10f050d46fb65b55059b068cbecd6e64d358e86abda0114de18fa557e2f1bba7/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f5265636f696c2d3335373845353f7374796c653d666f722d7468652d6261646765266c6f676f3d7265636f696c266c6f676f436f6c6f723d666666666666",
+      "https://camo.githubusercontent.com/c463b588bbeed15b8afeffa108586b606b02dfff31baec5063a5ee8ccd0bd419/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742051756572792d4646343135343f7374796c653d666f722d7468652d6261646765266c6f676f3d72656163747175657279266c6f676f436f6c6f723d666666666666",
     ],
     feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+      "Firebase와 연동한 차트",
+      "로그인, 회원가입, 프로필 수정",
+      "상품, SNS게시물, 댓글 CRUD 및 검색",
+      "무한 스크롤",
+      "모바일 반응형 X",
+    ],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "MapleStory Open API",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -31,16 +41,14 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "Crypto Coin Tracker",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -48,16 +56,14 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "Nomflix",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -65,16 +71,14 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "Richter Award",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -82,16 +86,14 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "G-Clef",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -99,16 +101,14 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
   },
   {
-    viewImg: "/bannerbg.jpeg",
+    viewImg: [
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+      "https://res.cloudinary.com/dgmgeotyk/image/upload/f_auto,q_auto/v1/Wings/h3qku2lbphxeyjbrtrdj",
+    ],
     siteName: "Surroundio",
     siteIntro: "사이트 간단 소개글",
     stackImg: [
@@ -116,12 +116,6 @@ export const portfolioList = [
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
       "https://camo.githubusercontent.com/f2354c79ec6bfb9ba6139a72bbc75c8c3a51a63801efdb6961d7a146b08e112d/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f52656163742d3631444146423f7374796c653d666f722d7468652d6261646765266c6f676f3d7265616374266c6f676f436f6c6f723d303030303030",
     ],
-    feature: [
-      "feature 1",
-      "feature 2",
-      "feature 3",
-      "feature 4",
-      "feature 5"
-    ]
-  }
+    feature: ["feature 1", "feature 2", "feature 3", "feature 4", "feature 5"],
+  },
 ];
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -25,13 +25,42 @@ const boxVar = {
     },
   },
 };
+
+const viewVar = {
+  entry: {
+    x: 40,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+  center: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+    },
+  },
+  hide: {
+    x: -40,
+    opacity: 0,
+    transition: {
+      duration: 0.6,
+    },
+  },
+};
 const Portfolio = () => {
   const ref2 = useRef(null);
   const inview = useInView(ref2, { once: true });
   const [currentPage, set_currentPage] = useState(0);
+  const [viewPage, set_viewPage] = useState(0);
   const nextCard = (imgLength) => {
     //다음 버튼 클릭
     set_currentPage((prev) => (prev === imgLength - 1 ? 0 : prev + 1)); //다음 숫자로 변경하여 페이지 넘김. 가장 끝일 경우 동작하지 않도록, 개수는 0부터 시작하지 않으므르 -1
+    set_viewPage(0);
+  };
+  const nextView = (imgLength) => {
+    set_viewPage((prev) => (prev === imgLength - 1 ? 0 : prev + 1)); 
   };
   return (
     <div className="portfolio-container">
@@ -62,7 +91,35 @@ const Portfolio = () => {
                   exit="hide"
                 >
                   <div className="portfolio-slide-img">
-                    <img src={data.viewImg} alt="" />
+                    <img className="macbook-img" src="/macbook.webp" alt="" />
+                    <div
+                      className="macbook-img-slider"
+                    >
+                      <div>
+                        <AnimatePresence mode="sync">
+                          {data.viewImg.map((img, num) => (
+                            num === viewPage && (
+                            <motion.img
+                              src={img}
+                              alt=""
+                              key={num}
+                              variants={viewVar}
+                              initial="entry"
+                              animate="center"
+                              exit="hide"
+                              onClick={() =>
+                                window.open(
+                                  "https://the-market-omega.vercel.app/",
+                                  "_blank"
+                                )
+                              }
+                            />
+                            )
+                          ))}
+                        </AnimatePresence>
+                        <button onClick={() => nextView(data.viewImg.length)}>next</button>
+                      </div>
+                    </div>
                   </div>
                   <div className="portfolio-slide-text">
                     <h2>{data.siteName}</h2>
@@ -88,20 +145,19 @@ const Portfolio = () => {
           className="portfolio-slider-button"
           onClick={() => nextCard(portfolioList.length)}
         >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1.5"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
-              />
-            </svg>
-         
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3"
+            />
+          </svg>
           Next
         </div>
       </motion.div>
