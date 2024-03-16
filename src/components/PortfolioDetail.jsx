98774d90e4d8aef537f31436b169ff3b065b0cee
JunDemi <jungwook3176@gmail.com>
2024-03-16 12:38:46 +0900

    Style: macbook이미지 절대위치로 지정

--- src/components/PortfolioDetail.jsx
+++ src/components/PortfolioDetail.jsx
@@ -1,19 +1,14 @@
 import { motion } from "framer-motion";
 import React from "react";
+import { portfolioList } from "../Utils";
 
 const PortfolioDetail = (page) => {
   return (
-    <motion.div className="portfolio-modal" 
-    layoutId={`macbook${page.page}`}>
-      <motion.img
-      className="macbook-device"
-        src="/bg/macbook.png"
-        alt=""
-      />
-      <div className="macbook-view">
-      <img
-       src="/the-market/themarket1.png" alt=""/>
-      </div>
+    <motion.div className="portfolio-modal" layoutId={`macbook${page.page}`}>
+      <motion.img className="macbook-device" src="/bg/macbook.png" alt="" />
+      {portfolioList[page].viewImg.map((src, number) => (
+        <img key={number} className="macbook-view" src={src} alt="" />
+      ))}
     </motion.div>
   );
 };
