f7eef9f61092b57d27f123853f3b39f166be9705
JunDemi <jungwook3176@gmail.com>
2024-03-19 12:56:28 +0900

    Feat: 디테일 모달 창 이미지 토글 버튼

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,8 +1,8 @@
 import { AnimatePresence, motion, useInView } from "framer-motion";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { portfolioList } from "../Utils";
 import { Parallax } from "react-parallax";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PortfolioDetail from "./PortfolioDetail";
 
 const boxVar = {
@@ -55,7 +55,6 @@ const viewVar = {
 };
 const Portfolio = () => {
   const router = useNavigate();
-  const pathname = useLocation().pathname.replace("/", "");
   const ref2 = useRef(null);
   const inview = useInView(ref2, { once: true });
   const [currentPage, set_currentPage] = useState(0);
@@ -63,13 +62,6 @@ const Portfolio = () => {
   const [viewPage, set_viewPage] = useState(0);
   const [overlay, set_overlay] = useState(false);
 
-  useEffect(()=> {
-    if(pathname === ""){
-      set_overlay(false);
-    }else{
-      set_overlay(true);
-    }
-  }, [pathname]);
   const currentPageSet = (current) => {
     if (currentPage > current) {
       set_back(true);
@@ -80,18 +72,6 @@ const Portfolio = () => {
     }
     set_currentPage(current);
   };
-  // const nextCard = (imgLength) => {
-  //   //다음 버튼 클릭
-  //   set_back(false);
-  //   set_currentPage((prev) => (prev === imgLength - 1 ? 0 : prev + 1)); //다음 숫자로 변경하여 페이지 넘김. 가장 끝일 경우 동작하지 않도록, 개수는 0부터 시작하지 않으므르 -1
-  //   set_viewPage(0);
-  // };
-  // const prevCard = (imgLength) => {
-  //   //이전 버튼 클릭
-  //   set_back(true);
-  //   set_currentPage((prev) => (prev === 0 ? imgLength - 1 : prev - 1)); //이전 숫자로 변경하여 페이지 넘김
-  //   set_viewPage(0);
-  // };
   const nextView = (imgLength) => {
     set_viewPage((prev) => (prev === imgLength - 1 ? 0 : prev + 1));
   };
@@ -99,173 +79,183 @@ const Portfolio = () => {
     //이전 버튼 클릭
     set_viewPage((prev) => (prev === 0 ? imgLength - 1 : prev - 1)); //이전 숫자로 변경하여 페이지 넘김
   };
+
+  const getOverlay = (siteName) => {
+    set_overlay(true);
+    router(`/${siteName}`);
+  };
+  const closeOverlay = () => {
+    set_overlay(false);
+    router("/");
+  };
   return (
     <>
-    <div className="portfolio-container">
-      <Parallax
-        strength={350}
-        bgImage="/bg/portfoliobg.png"
-        className="portfolio-bg"
-      />
-      <div className="portfolio-content">
-        <motion.h1
-          initial={{ opacity: 0, x: -100 }}
-          animate={inview ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
-          transition={{ delay: 0.3, ease: "easeOut" }}
-        >
-          Portfolio
-        </motion.h1>
-        <motion.div
-          className="portfolio-slider"
-          initial={{ opacity: 0, x: 150 }}
-          animate={inview ? { opacity: 1, x: 0 } : { opacity: 0, x: 150 }}
-          transition={{ delay: 0.3, ease: "easeOut" }}
-        >
-          <AnimatePresence mode="sync" custom={back}>
-            {portfolioList.map(
-              (data, page) =>
-                page === currentPage && (
-                  <motion.div
-                    className="portfolio-slide"
-                    key={page}
-                    custom={back}
-                    variants={boxVar}
-                    initial="entry"
-                    animate="center"
-                    exit="hide"
-                    transition={{ delay: 0.3 }}
-                  >
-                    <motion.div 
-                    className="portfolio-slide-img"
-                    layoutId={`macbook${page}`}
+      <div className="portfolio-container">
+        <Parallax
+          strength={350}
+          bgImage="/bg/portfoliobg.png"
+          className="portfolio-bg"
+        />
+        <div className="portfolio-content">
+          <motion.h1
+            initial={{ opacity: 0, x: -100 }}
+            animate={inview ? { opacity: 1, x: 0 } : { opacity: 0, x: -100 }}
+            transition={{ delay: 0.3, ease: "easeOut" }}
+          >
+            Portfolio
+          </motion.h1>
+          <motion.div
+            className="portfolio-slider"
+            initial={{ opacity: 0, x: 150 }}
+            animate={inview ? { opacity: 1, x: 0 } : { opacity: 0, x: 150 }}
+            transition={{ delay: 0.3, ease: "easeOut" }}
+          >
+            <AnimatePresence mode="sync" custom={back}>
+              {portfolioList.map(
+                (data, page) =>
+                  page === currentPage && (
+                    <motion.div
+                      className="portfolio-slide"
+                      key={page}
+                      custom={back}
+                      variants={boxVar}
+                      initial="entry"
+                      animate="center"
+                      exit="hide"
+                      transition={{ delay: 0.3 }}
                     >
-                      <motion.img
-                        className="macbook-img"
-                        src="/bg/macbook.png"
-                        alt=""
-                      />
-                      <div className="macbook-img-slider">
-                        <div className="view-slider">
-                          <AnimatePresence mode="sync">
-                            {data.viewImg.map(
-                              (img, num) =>
-                                num === viewPage && (
-                                  <motion.img
-                                    src={img}
-                                    alt=""
-                                    key={num}
-                                    variants={viewVar}
-                                    initial="entry"
-                                    animate="center"
-                                    exit="hide"
-                                    onClick={()=>router(`/${portfolioList[page].siteName}`)}
-                                  />
-                                )
-                            )}
-                          </AnimatePresence>
-                          <div className="view-page-button">
-                            <svg
-                              onClick={() => prevView(data.viewImg.length)}
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15.75 19.5 8.25 12l7.5-7.5"
-                              />
-                            </svg>
-                            <span>
-                              {viewPage + 1}/{data.viewImg.length}
-                            </span>
-                            <svg
-                              onClick={() => nextView(data.viewImg.length)}
-                              xmlns="http://www.w3.org/2000/svg"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m8.25 4.5 7.5 7.5-7.5 7.5"
-                              />
-                            </svg>
+                      <motion.div className="portfolio-slide-img">
+                        <motion.img
+                          className="macbook-img"
+                          src="/bg/macbook.png"
+                          alt=""
+                        />
+                        <div className="macbook-img-slider">
+                          <div className="view-slider">
+                            <AnimatePresence mode="sync">
+                              {data.viewImg.map(
+                                (img, num) =>
+                                  num === viewPage && (
+                                    <motion.img
+                                      src={img}
+                                      alt=""
+                                      key={num}
+                                      variants={viewVar}
+                                      initial="entry"
+                                      animate="center"
+                                      exit="hide"
+                                      onClick={() => getOverlay(data.siteName)}
+                                    />
+                                  )
+                              )}
+                            </AnimatePresence>
+                            <div className="view-page-button">
+                              <svg
+                                onClick={() => prevView(data.viewImg.length)}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M15.75 19.5 8.25 12l7.5-7.5"
+                                />
+                              </svg>
+                              <span>
+                                {viewPage + 1}/{data.viewImg.length}
+                              </span>
+                              <svg
+                                onClick={() => nextView(data.viewImg.length)}
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m8.25 4.5 7.5 7.5-7.5 7.5"
+                                />
+                              </svg>
+                            </div>
                           </div>
+                        </div>
+                      </motion.div>
+                      <div className="portfolio-slide-text">
+                        <div>
+                          <Link to={data.link} target="_blank">
+                            {data.siteName}
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                              stroke="currentColor"
+                            >
+                              <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+                              />
+                            </svg>
+                          </Link>
+                        </div>
+                        <h3>{data.siteIntro}</h3>
+                        <div className="portfolio-slide-stackImg">
+                          {data.stackImg.map((img, num) => (
+                            <img key={num} src={img} alt="" />
+                          ))}
+                        </div>
+                        <div className="feature-list">
+                          <h4>Feature</h4>
+                          {data.feature.map((text, num) => (
+                            <p key={num}>{text}</p>
+                          ))}
                         </div>
                       </div>
                     </motion.div>
-                    <div className="portfolio-slide-text">
-                      <div>
-                        <Link to={data.link} target="_blank">
-                          {data.siteName}
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                          >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
-                            />
-                          </svg>
-                        </Link>
-                      </div>
-                      <h3>{data.siteIntro}</h3>
-                      <div className="portfolio-slide-stackImg">
-                        {data.stackImg.map((img, num) => (
-                          <img key={num} src={img} alt="" />
-                        ))}
-                      </div>
-                      <div className="feature-list">
-                        <h4>Feature</h4>
-                        {data.feature.map((text, num) => (
-                          <p key={num}>{text}</p>
-                        ))}
-                      </div>
-                    </div>
-                  </motion.div>
-                )
-            )}
-          </AnimatePresence>
-        </motion.div>
-        <motion.div
-          className="slide-progress-bar"
-        >
-          {[...Array(portfolioList.length)].map((data, number) => (
-            <>
-              <motion.button
-                ref={ref2}
-                onClick={() => currentPageSet(number)}
-                initial={{ opacity: 0, scale: 0 }}
-                animate={inview ? {opacity: 1, scale: 1} : {opacity: 0, scale: 0}}
-                transition={{ delay: 0.8 + number / 10 * 1.5}}
-                key={number}
-                style={
-                  currentPage === number
-                    ? {
-                        width: "50px",
-                        height: "50px",
-                        backgroundColor: "#fff",
-                        color: "#000",
-                        fontSize: "16px",
-                      }
-                    : {}
-                }
-              >
-                {number + 1}
-              </motion.button>
-              <motion.hr initial={{ opacity: 0, scale: 0 }}
-                animate={inview ? {opacity: 1, scale: 1} : {opacity: 0, scale: 0}}
-                transition={{ delay: 0.8 + number / 10 * 1.5}}/>
-                </>
-          ))}
-        </motion.div>
-        {/* <motion.div className="portfolio-slide-button"
+                  )
+              )}
+            </AnimatePresence>
+          </motion.div>
+          <motion.div className="slide-progress-bar">
+            {[...Array(portfolioList.length)].map((data, number) => (
+              <>
+                <motion.button
+                  ref={ref2}
+                  onClick={() => currentPageSet(number)}
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={
+                    inview ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }
+                  }
+                  transition={{ delay: 0.8 + (number / 10) * 1.5 }}
+                  key={number}
+                  style={
+                    currentPage === number
+                      ? {
+                          width: "50px",
+                          height: "50px",
+                          backgroundColor: "#fff",
+                          color: "#000",
+                          fontSize: "16px",
+                        }
+                      : {}
+                  }
+                >
+                  {number + 1}
+                </motion.button>
+                <motion.hr
+                  initial={{ opacity: 0, scale: 0 }}
+                  animate={
+                    inview ? { opacity: 1, scale: 1 } : { opacity: 0, scale: 0 }
+                  }
+                  transition={{ delay: 0.8 + (number / 10) * 1.5 }}
+                />
+              </>
+            ))}
+          </motion.div>
+          {/* <motion.div className="portfolio-slide-button"
         initial={{ opacity: 0 }}
         animate={inview ? { opacity: 1 } : { opacity: 0 }}
         transition={{ delay: 0.3, ease: "easeOut" }}>
@@ -321,21 +311,39 @@ const Portfolio = () => {
             Next
           </motion.div>
         </motion.div> */}
+        </div>
       </div>
-    </div>
-    {overlay && 
-    
-    <AnimatePresence>
-    <motion.div
-      className="overlay-dark"
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-    >
-     <PortfolioDetail pages={currentPage} viewImg={portfolioList[currentPage].viewImg}/>
-     </motion.div>
-     </AnimatePresence>
-    }
+      <AnimatePresence>
+        {overlay && (
+          <motion.div
+            className="overlay-dark"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+              <motion.svg
+              className="close_btn" onClick={closeOverlay}
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                whileHover={{rotateZ: 90}}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </motion.svg>
+            <PortfolioDetail
+              pages={currentPage}
+              viewImg={portfolioList[currentPage].viewImg}
+              link={portfolioList[currentPage].link}
+            />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </>
   );
 };
--- src/components/PortfolioDetail.jsx
+++ src/components/PortfolioDetail.jsx
@@ -1,9 +1,7 @@
 import { motion } from "framer-motion";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 const PortfolioDetail = (props) => {
-  const router = useNavigate();
   const [viewPage, set_viewPage] = useState(0);
 
   const nextView = (viewLength) => {
@@ -15,37 +13,61 @@ const PortfolioDetail = (props) => {
   };
   return (
     <>
-      <motion.div
-        className="portfolio-modal"
-        layoutId={`macbook${props.pages}`}
-      >
+      <motion.div className="portfolio-modal">
         <motion.img className="macbook-device" src="/bg/macbook.png" alt="" />
         {props.viewImg.map(
           (data, number) =>
             number === viewPage && (
-              <img className="macbook-view" src={data} alt="" key={number} />
+              <img className="macbook-view" src={data} alt="" key={number} 
+              onClick={() => window.open(props.link, "_blank")}/>
             )
         )}
-        {/* <button
+      </motion.div>
+      <div className="detail-slide-button">
+        <motion.button
           className="prev_btn"
           onClick={() => prevView(props.viewImg.length)}
+          whileTap={{x: -7}}
         >
-          prev
-        </button>
-        <button
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m18.75 4.5-7.5 7.5 7.5 7.5m-6-15L5.25 12l7.5 7.5"
+            />
+          </svg>
+        </motion.button>
+        {props.viewImg.map((img, num) => (
+          <motion.img
+            src={img}
+            alt=""
+            onClick={() => set_viewPage(num)}
+            animate={viewPage === num ? { width: "100px" } : { width: "60px" }}
+          />
+        ))}
+        <motion.button
           className="next_btn"
           onClick={() => nextView(props.viewImg.length)}
+          whileTap={{x: 7}}
         >
-          next
-        </button> */}
-      </motion.div>
-      <button className="close_btn" onClick={() => router("/")}>
-        close
-      </button>
-      <div className="detail-slide-button">
-        {props.viewImg.map((img, num) => (
-          <img src={img} alt="" width="100px" />
-        ))}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </motion.button>
       </div>
     </>
   );
