1f86f755cf8286c02c1a2c5263193121066c3939
JunDemi <jungwook3176@gmail.com>
2024-03-29 15:30:35 +0900

    Style: 포트폴리오 배경변경, 맥북 이미지 변경

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -248,8 +248,7 @@ const Portfolio = () => {
                       ? {
                           width: "50px",
                           height: "50px",
-                          backgroundColor: "#fff",
-                          color: "#000",
+                          color: "#61cfff",
                           fontSize: "16px",
                           fontFamily: "SUITE-bold"
                         }
